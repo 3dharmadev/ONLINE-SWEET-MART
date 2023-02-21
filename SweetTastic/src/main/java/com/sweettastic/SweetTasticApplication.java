9e1b2d8ddf03d8f1301976e052c14ac64bb8c232
3dharmadev <69399810+3dharmadev@users.noreply.github.com>
2023-02-22 01:35:47 +0530

    project skeleton added

---
+++ SweetTastic/src/main/java/com/sweettastic/SweetTasticApplication.java
@@ -0,0 +1,13 @@
+package com.sweettastic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SweetTasticApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SweetTasticApplication.class, args);
+	}
+
+}
