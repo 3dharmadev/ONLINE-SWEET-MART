d72dbc47f5a4ca63eea2b08201f732f050d64a68
Anirudha Desai <desaianu90@gmail.com>
2023-02-23 00:06:20 +0530

    modules added to repository

---
+++ Sweettastic Backend/Sweettastic/src/test/java/com/sweettastic/SweettasticApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sweettastic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SweettasticApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
