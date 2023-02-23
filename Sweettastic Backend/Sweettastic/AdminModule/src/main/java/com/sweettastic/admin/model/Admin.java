ac416bd5829cd48bd8b5f02c99c64face843cada
3dharmadev <69399810+3dharmadev@users.noreply.github.com>
2023-02-23 18:12:28 +0530

    added SweetOrder Module

--- Sweettastic Backend/Sweettastic/AdminModule/src/main/java/com/sweettastic/admin/model/Admin.java
+++ Sweettastic Backend/Sweettastic/AdminModule/src/main/java/com/sweettastic/admin/model/Admin.java
@@ -2,14 +2,10 @@ package com.sweettastic.admin.model;
 
 import java.util.List;
 
-import com.sweettastic.cart.model.Cart;
 import com.sweettastic.category.model.Category;
-import com.sweettastic.category.model.Customer;
 import com.sweettastic.product.model.Product;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
@@ -23,9 +19,7 @@ import lombok.NoArgsConstructor;
 @Entity
 public class Admin {
 
-	
 	@Id
-	@GeneratedValue(strategy = GenerationType.SEQUENCE)
 	private Integer id;
 	
 	@NotNull
--- Sweettastic Backend/Sweettastic/ProductModule/src/main/java/com/sweettastic/product/model/Product.java
+++ Sweettastic Backend/Sweettastic/ProductModule/src/main/java/com/sweettastic/product/model/Product.java
@@ -40,4 +40,69 @@ public class Product {
 	
 	@NotNull
 	private Category category;
+
+	public Integer getProductid() {
+		return productid;
+	}
+
+	public void setProductid(Integer productid) {
+		this.productid = productid;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPhotopath() {
+		return photopath;
+	}
+
+	public void setPhotopath(String photopath) {
+		this.photopath = photopath;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Boolean getAvailable() {
+		return available;
+	}
+
+	public void setAvailable(Boolean available) {
+		this.available = available;
+	}
+
+	public Category getCategory() {
+		return category;
+	}
+
+	public void setCategory(Category category) {
+		this.category = category;
+	}
+
+	@Override
+	public String toString() {
+		return "Product [productid=" + productid + ", name=" + name + ", photopath=" + photopath + ", price=" + price
+				+ ", description=" + description + ", available=" + available + ", category=" + category + "]";
+	}
+	
+	
+	
 }
