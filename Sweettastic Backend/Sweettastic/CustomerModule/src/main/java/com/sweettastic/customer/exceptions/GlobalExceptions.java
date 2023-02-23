13d0b7f2b2051d15347b3a417ed5277547098bbc
SurajGundle <shubhamgundle35@gmail.com>
2023-02-23 14:50:15 +0530

    pushed to main temp

--- Sweettastic Backend/Sweettastic/CustomerModule/src/main/java/com/sweettastic/customer/exceptions/GlobalExceptions.java
+++ Sweettastic Backend/Sweettastic/CustomerModule/src/main/java/com/sweettastic/customer/exceptions/GlobalExceptions.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.NoHandlerFoundException;
 
-import jakarta.validation.ConstraintViolation;
 import jakarta.validation.ConstraintViolationException;
 
 @ControllerAdvice
