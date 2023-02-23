bcb13b7e0b089b648d34226e547a11c5dc06acdb
Anirudha Desai <desaianu90@gmail.com>
2023-02-23 12:21:12 +0530

    modified

--- Sweettastic Backend/Sweettastic/src/main/java/com/sweettastic/SweettasticApplication.java
+++ Sweettastic Backend/Sweettastic/src/main/java/com/sweettastic/SweettasticApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.sweettastic.admin", "com.sweettastic.cart", "com.sweettastic.category", "com.sweettastic.customer", "com.sweettastic.orderbill", "com.sweettastic.product", "com.sweettastic.sweetorder", "com.sweettastic.user"})
+@EntityScan(basePackages = {"com.sweettastic.admin", "com.sweettastic.cart", "com.sweettastic.category", "com.sweettastic.customer", "com.sweettastic.orderbill", "com.sweettastic.product", "com.sweettastic.sweetorder", "com.sweettastic.user"})
+@EnableJpaRepositories(basePackages = {"com.sweettastic.admin", "com.sweettastic.cart", "com.sweettastic.category", "com.sweettastic.customer", "com.sweettastic.orderbill", "com.sweettastic.product", "com.sweettastic.sweetorder", "com.sweettastic.user"})
 public class SweettasticApplication {
 
 	public static void main(String[] args) {
