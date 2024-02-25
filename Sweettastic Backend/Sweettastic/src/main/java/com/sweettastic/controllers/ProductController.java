22593dcb6ccd0340cb51deeb9ca29d31688c8aab
Debabrata Satapathy <69399810+3dharmadev@users.noreply.github.com>
2024-02-25 20:41:21 +0530

    Update ProductController.java

--- Sweettastic Backend/Sweettastic/src/main/java/com/sweettastic/controllers/ProductController.java
+++ Sweettastic Backend/Sweettastic/src/main/java/com/sweettastic/controllers/ProductController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-@RequestMapping("/product")
+@RequestMapping("/products")
 public class ProductController {
 
 	@Autowired
