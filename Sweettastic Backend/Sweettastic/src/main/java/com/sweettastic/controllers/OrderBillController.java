a0c0442afa3c37b7380071290c19b9121af11465
Debabrata Satapathy <69399810+3dharmadev@users.noreply.github.com>
2024-02-25 20:41:00 +0530

    Update OrderBillController.java

--- Sweettastic Backend/Sweettastic/src/main/java/com/sweettastic/controllers/OrderBillController.java
+++ Sweettastic Backend/Sweettastic/src/main/java/com/sweettastic/controllers/OrderBillController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/orderbill")
+@RequestMapping("/orderbills")
 public class OrderBillController {
 
     @Autowired
